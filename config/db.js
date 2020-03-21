911ce341134c34a3ba1987a4d93c8a6032e14148
Frank Savage <gotsavaged@gmail.com>
2020-03-21 16:39:50 -0700

    trying to fix index.js and connection

--- config/db.js
+++ config/db.js
@@ -1,15 +1,18 @@
 const { createConnection } = require('mysql2');
-
+const app = express()
 const connection = createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: 'rootroot',
   database: 'myemployee_db'
-});
-// app.listen(3001, () => {
-//   console.log("server is up and running")
-// })
+})
+{
+app.set('port', process.env.PORT || 3003);
+app.listen(3003, () => {
+  console.log("server is up and running")
+})
+}
 
 
 module.exports = connection
--- index.js
+++ index.js
@@ -1,109 +1,113 @@
 const express = require('express');
 const app = express();
-const db = require('./config/db.js');
+// const db = require('./config/db.js');
 const prompt = require('inquirer').createPromptModule();
 const mysql = require('mysql');
 const consoletable = require('console.table');
 const join = require('path');
 
-myemployee = () => {
+app.listen(3003, () => {
+  console.log("server is up and running")
+},
 
-  prompt({
-    type: 'list',
-    name: 'options',
-    message: "What would you like to do?",
-    choices: ['View All Employees', 'View All Departments', 'View Roles', 'Add Employee', 'Add Role', 'Add Department', 'Update Employee Role']
-  })
-    .then((options) => {
+  myemployee = () => {
 
-      switch (options) {
-        case 'View All Employees':
-          viewAllEmployees()
-          break;
-        case 'View All Departments':
-          viewDepartments()
-          break;
-        case 'View Roles':
-          viewRoles()
-          break;
-        case 'Add Employee':
-          addEmployee()
-          break;
-        case 'Add Role':
-          addRole()
-          break;
-        case 'Add Department':
-          addDepartment()
-          break;
-        case 'Update Employee Role':
-          updateRole()
-          break;
-      }
-    })
-    .catch(e => console.log(e))
-
-
-}
-
-viewAllEmployees = () => {
-  const query = 'SELECT * FROM employee';
-  connection.query(query, (err, res) => {
-    if (err) return err
-    console.table(res)
-    myemployee()
-  })
-}
-
-viewDepartment = () => {
-  const query = 'SELECT * FROM department'
-  connection.query(query, (err, res) => {
-    console.table(res)
-    myemployee()
-  })
-}
-
-viewRoles = () => {
-  const query = 'SELECT * FROM role'
-  connection.query(query, (err, res) => {
-    console.table(res)
-    myemployee()
-  })
-}
-
-addEmployee = () => {
-  prompt(
-    {
-      type: 'input',
-      name: 'first_name',
-      message: 'First Name:'
-    },
-    {
-      type: 'input',
-      name: 'last_name',
-      message: 'Last Name:'
-    },
-    {
+    prompt({
       type: 'list',
-      name: 'department',
-      message: 'Department:',
-      choices: ['Development', 'Implementation', 'Research', 'Training', 'Production']
-    },
-    {
-      type: 'input',
-      name: 'role title',
-      message: "Enter employee's role:",
-      choices: ['Software Engineer', 'Research Analyst', 'Junior Developer', 'Intern', 'Tech Support Specialist', 'Account Executive', 'Senior Developer', 'Data Analyst', 'UI UX Designer',]
-    },
-    {
-      type: 'input',
-      name: 'manager',
-      message: "Enter employee's manager:"
-    }
-  )
-}
+      name: 'options',
+      message: "What would you like to do?",
+      choices: ['View All Employees', 'View All Departments', 'View Roles', 'Add Employee', 'Add Role', 'Add Department', 'Update Employee Role']
+    })
+      .then((options) => {
 
-myemployee()
+        switch (options) {
+          case 'View All Employees':
+            viewAllEmployees()
+            break;
+          case 'View All Departments':
+            viewDepartments()
+            break;
+          case 'View Roles':
+            viewRoles()
+            break;
+          case 'Add Employee':
+            addEmployee()
+            break;
+          case 'Add Role':
+            addRole()
+            break;
+          case 'Add Department':
+            addDepartment()
+            break;
+          case 'Update Employee Role':
+            updateRole()
+            break;
+        }
+      })
+      .catch(e => console.log(e))
 
-function newFunction(options) {
-  console.table(options)
-}
+
+  }),
+
+  viewAllEmployees = () => {
+    const query = 'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+      if (err) return err
+      console.table(res)
+      myemployee()
+    })
+  },
+
+  viewDepartment = () => {
+    const query = 'SELECT * FROM department'
+    connection.query(query, (err, res) => {
+      console.table(res)
+      myemployee()
+    })
+  },
+
+  viewRoles = () => {
+    const query = 'SELECT * FROM role'
+    connection.query(query, (err, res) => {
+      console.table(res)
+      myemployee()
+    })
+  },
+
+  addEmployee = () => {
+    prompt(
+      {
+        type: 'input',
+        name: 'first_name',
+        message: 'First Name:'
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: 'Last Name:'
+      },
+      {
+        type: 'list',
+        name: 'department',
+        message: 'Department:',
+        choices: ['Development', 'Implementation', 'Research', 'Training', 'Production']
+      },
+      {
+        type: 'input',
+        name: 'role title',
+        message: "Enter employee's role:",
+        choices: ['Software Engineer', 'Research Analyst', 'Junior Developer', 'Intern', 'Tech Support Specialist', 'Account Executive', 'Senior Developer', 'Data Analyst', 'UI UX Designer',]
+      },
+      {
+        type: 'input',
+        name: 'manager',
+        message: "Enter employee's manager:"
+      }
+    )
+  },
+
+  myemployee()
+
+  function newFunction(options) {
+    console.table(options);
+  }
